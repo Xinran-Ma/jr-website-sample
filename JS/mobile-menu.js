7ac535da60ed83a1d01fe98a0e5e7960be84cc1b
Ray Ma <xinran_ma@hotmail.com>
2021-12-04 13:03:32 +1100

    Fixed footer layout issue. Fixed convert-svg.js loading issue. Implemented ES5 instead of ES6.

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,17 +1,17 @@
-const openMobileMenu = () => {
-    const mobileMenuWrapper = document.querySelector(".header__mobile-menu-wrapper");
+function openMobileMenu() {
+    var mobileMenuWrapper = document.querySelector(".header__mobile-menu-wrapper");
     mobileMenuWrapper.classList.remove("closed");
     mobileMenuWrapper.classList.add("opened");
 }
 
-const closeMobileMenu = () => {
-    const mobileMenuWrapper = document.querySelector(".header__mobile-menu-wrapper");
+function closeMobileMenu() {
+    var mobileMenuWrapper = document.querySelector(".header__mobile-menu-wrapper");
     mobileMenuWrapper.classList.remove("opened");
     mobileMenuWrapper.classList.add("closed");
 }
 
-const toggleSearchField = () => {
-    const mobileSearchBar = document.querySelector(".header__mobile-search-area");
+function toggleSearchField() {
+    var mobileSearchBar = document.querySelector(".header__mobile-search-area");
     if(!mobileSearchBar.classList.contains("opened")) {
         mobileSearchBar.classList.add("opened")
         mobileSearchBar.classList.remove("closed")
